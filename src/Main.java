b5fff65fe6cd263a61a8364033795e91828385b1
Davide Corsi <davide.corsi@par-tec.it>
2021-10-04 09:58:56 +0200

    risoluzione

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,40 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Inserisci una stringa ");
 		String s = input.nextLine();
+		int a = 0;
+		int e = 0;
+		int i = 0;
+		int o = 0;
+		int u = 0;
+		for(int j = 0; j < s.length(); j++) {
+			char c = s.charAt(j);
+			switch(c) {
+			case 'A':
+			case 'a':
+				a++;
+				break;
+			case 'E':
+			case 'e':
+				e++;
+				break;
+			case 'I':
+			case 'i':
+				i++;
+				break;
+			case 'O':
+			case 'o':
+				o++;
+				break;
+			case 'U':
+			case 'u':
+				u++;
+				break;
+			}
+		}
+		System.out.println("Il numero di occorrenze di A è " + a);
+		System.out.println("Il numero di occorrenze di E è " + e);
+		System.out.println("Il numero di occorrenze di I è " + i);
+		System.out.println("Il numero di occorrenze di O è " + o);
+		System.out.println("Il numero di occorrenze di U è " + u);
 	}
 }
