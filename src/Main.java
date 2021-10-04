4cb4ac2750db13446265b91a2b575a5acdb0ebe9
Davide Corsi <davide.corsi@par-tec.it>
2021-10-04 09:57:21 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/*
+ * Scrivete un applicazione che chieda all'utente una stringa e calcoli il numero di occorrenze di ogni
+ * vocale.
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Inserisci una stringa ");
+		String s = input.nextLine();
+	}
+}
